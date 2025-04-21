81fce18f98357bf347f86b1b13b858210d83d064
chohyeokjun <chj3731@naver.com>
2025-04-21 10:43:24 +0900

    refactor: Lv5 todo가 정상적으로 등록된다() 메서드가 정상적으로 작동하기 위해 ManagerService에서 두 번째 조건문에 !를 삽입하여 수정

--- src/main/java/org/example/expert/domain/manager/service/ManagerService.java
+++ src/main/java/org/example/expert/domain/manager/service/ManagerService.java
@@ -41,7 +41,7 @@ public class ManagerService {
         }
 
 
-        if (ObjectUtils.nullSafeEquals(user.getId(), todo.getUser().getId())) {
+        if (!ObjectUtils.nullSafeEquals(user.getId(), todo.getUser().getId())) {
             throw new InvalidRequestException("담당자를 등록하려고 하는 유저가 일정을 만든 유저가 유효하지 않습니다.");
         }
 
